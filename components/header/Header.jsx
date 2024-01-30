6991ecb49943618e3af6b54a100cfbae5604a6f1
Rley <ffast2000@gmail.com>
2024-01-30 15:58:59 -0500

    adding color theme in header.css

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -1,23 +1,31 @@
 import './header.css'
+import logo from '../../public/potrait.webp'
 function Header() {
 
   const links = ['Home', 'About', 'Blog', 'Projects', 'Contact']
   const social = [
     {
       icon: "fa-brands fa-github",
-      link : 'https://github.com/10d3'
+      link: 'https://github.com/10d3'
     },
     {
       icon: "fa-brands fa-linkedin",
-      link : 'https://www.linkedin.com/in/aherleym/'
+      link: 'https://www.linkedin.com/in/aherleym/'
+    },
+    {
+      icon: "fa-brands fa-whatsapp",
+      link: "https://wa.me/50944585449"
+    },
+    {
+      icon: "fa-solid fa-inbox",
+      link: ""
     }
   ]
 
   return (
     <div className="main-header">
       <div className='img-header'>
-        <img src="" alt="" />
-        <h2>image here</h2>
+        <img src={logo} alt="" />
       </div>
       <div className='nav'>
         <ul>
@@ -29,11 +37,11 @@ function Header() {
         </ul>
       </div>
       <div className='copyright'>
-          {
-            social.map((element, index) => {
-              return <a key={index} href={element.link}><i className={element.icon}></i></a>
-            })
-          }
+        {
+          social.map((element, index) => {
+            return <a key={index} href={element.link}><i className={element.icon}></i></a>
+          })
+        }
       </div>
     </div>
   )
