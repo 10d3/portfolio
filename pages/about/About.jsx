a243438adb2b4fc158256ebedcb453a3217ffa27
Rley <ffast2000@gmail.com>
2024-02-14 15:49:57 -0800

    adding bootstrapp in the projet

--- pages/about/About.jsx
+++ pages/about/About.jsx
@@ -1,26 +1,103 @@
-import './about.css'
-import { skills } from './skill'
-import imageP from '../../src/assets/imageP.png'
+import "./about.css";
+import { skills } from "./skill";
+import imageP from "../../src/assets/imageP.png";
 
 function About() {
-
   const info = {
-    about: 'About Me',
-    desc: ['In the realm of HTML, I meticulously structure content to ensure optimal accessibility and semantic clarity. With CSS, I bring designs to life, utilizing styles that not only enhance aesthetics but also contribute to a seamless user interface. My proficiency in JavaScript empowers me to add dynamic and interactive elements, enhancing the overall user engagement.',
-      'One of my focal points is leveraging React to build scalable and efficient user interfaces. This powerful library allows me to create modular components, fostering maintainability and enhancing the overall development process. I take pride in my ability to blend creativity with functionality, resulting in websites that not only meet but exceed user expectations.']
-  }
+    about: "About Me",
+    desc: [
+      "In the realm of HTML, I meticulously structure content to ensure optimal accessibility and semantic clarity. With CSS, I bring designs to life, utilizing styles that not only enhance aesthetics but also contribute to a seamless user interface. My proficiency in JavaScript empowers me to add dynamic and interactive elements, enhancing the overall user engagement.",
+      "One of my focal points is leveraging React to build scalable and efficient user interfaces. This powerful library allows me to create modular components, fostering maintainability and enhancing the overall development process. I take pride in my ability to blend creativity with functionality, resulting in websites that not only meet but exceed user expectations.",
+    ],
+  };
   return (
-    <div className="main-about">
-      <div className="sec-about">
-        <div className="sec-img">
-          <img src={imageP} alt="" />
+    <section id="about" className="my-5 py-5 bg-text" data-text="02">
+      <div className="row d-flex flex-wrap" data-aos="fade-up">
+        <div className="col-lg-6 pb-5">
+          <span className="text-muted text-uppercase">BIODATA</span>
+          <h2 className="display-4 txt-fx slide-up">Education</h2>
+          <div className="data-info py-3" data-aos="fade-up">
+            <span className="meta-date">1998 - 2004</span>
+            <h3 className="info-title">
+              Bachelors in Engineering in Information Technology
+            </h3>
+            <p>Harvard School of Science and management</p>
+          </div>
+          <div className="data-info" data-aos="fade-up" data-aos-delay="100">
+            <span className="meta-date">2004 - 2006</span>
+            <h3 className="info-title">Masters in Data Analysis</h3>
+            <p>Harvard School of Science and management</p>
+          </div>
+        </div>
+        <div className="col-lg-6 pb-5">
+          <h2 className="display-4 txt-fx slide-up">Experiences</h2>
+          <div
+            className="data-info py-3"
+            data-aos="fade-up"
+            data-aos-delay="200"
+          >
+            <span className="meta-date">2007 - 2012</span>
+            <h3 className="info-title">Creative Agency Inc.: Design head</h3>
+            <p>
+              iacentem substantiales um se sed esse haec Possit facis qui a a a
+              patriam.
+            </p>
+          </div>
+          <div className="data-info" data-aos="fade-up" data-aos-delay="300">
+            <span className="meta-date">2013 - present</span>
+            <h3 className="info-title">Studio Alpha.: Project Manager</h3>
+            <p>
+              iacentem substantiales um se sed esse haec Possit facis qui a a a
+              patriam.
+            </p>
+          </div>
         </div>
       </div>
-      <div className="sec-about">
-        what i do
+      <div className="row d-flex flex-wrap" data-aos="fade-up">
+        <div className="col-lg-6 pb-5">
+          <h2 className="display-4 txt-fx slide-up">Interests</h2>
+          <div className="data-info py-3" data-aos="fade-up">
+            <span className="meta-date">2007 - 2012</span>
+            <h3 className="info-title">
+              Bachelors in Engineering in Information Technology
+            </h3>
+            <p>Harvard School of Science and management</p>
+          </div>
+          <div className="data-info" data-aos="fade-up" data-aos-delay="100">
+            <span className="meta-date">2013 - present</span>
+            <h3 className="info-title">Studio Alpha.: Project Manager</h3>
+            <p>
+              iacentem substantiales um se sed esse haec Possit facis qui a a a
+              patriam.
+            </p>
+          </div>
+        </div>
+        <div className="col-lg-6 pb-5">
+          <h2 className="display-4 txt-fx slide-up">References</h2>
+          <div
+            className="data-info py-3"
+            data-aos="fade-up"
+            data-aos-delay="200"
+          >
+            <span className="meta-date">1998 - 2004</span>
+            <h3 className="info-title">Dr. Stephen H. King</h3>
+            <p>
+              iacentem substantiales um se sed esse haec Possit facis qui a a a
+              patriam.
+            </p>
+          </div>
+          <div className="data-info" data-aos="fade-up" data-aos-delay="300">
+            <span className="meta-date">2004 - 2006</span>
+            <h3 className="info-title">Dr. David Howard</h3>
+            <p>
+              iacentem substantiales um se sed esse haec Possit facis qui a a a
+              patriam.
+            </p>
+          </div>
+        </div>
       </div>
-    </div>
-  )
+    </section>
+  );
 }
 
-export default About
+export default About;
--- pages/home/Home.jsx
+++ pages/home/Home.jsx
@@ -1,36 +1,72 @@
-import './home.css'
-import { Link } from 'react-router-dom'
-import imageP from '../../src/assets/imageP.png'
+import "./home.css";
+import { Link } from "react-router-dom";
+import imageP from "../../src/assets/imageP.png";
 
 function Home() {
-
   const info = {
-    greet: 'Hi, my name is',
-    profession: 'Marc-Herley Antoine',
-    ptext: 'Herley'
-  }
+    greet: "Hi, my name is",
+    profession: "Marc-Herley Antoine",
+    ptext: "Herley",
+  };
   return (
-    <div className="main-home">
-      <div className='main-sub'>
-        <div className="sec-home">
-          <div className='img-pre'>
-          <img src={imageP} alt="" />
+    <div className="justify-content-center px-1 mx-1 px-xl-5 mx-xl-5">
+      <div className="py-5 bg-text">
+        <div className="row">
+          <div className="col-lg-6">
+            <div className="banner-image text-center" data-aos="fade-right">
+              <img src={imageP} alt="banner" className="img-fluid" />
+            </div>
+          </div>
+          <div className="col-lg-6">
+            <div
+              className="banner-content my-1 pt-1 my-lg-5 pt-lg-5"
+              data-aos="fade-in"
+            >
+              <span className="text-muted text-uppercase">
+                Designer / Developer
+              </span>
+              <h1 className="banner-title display-xl lh-1 txt-fx slide-up">
+                Marc-Herley Antoine
+              </h1>
+            </div>
           </div>
         </div>
-        <div className='sec-home'>
-          <div className='text-pre'>
-            <p>Hello, I&apos;m</p>
-            <h2>{info.profession}</h2>
-            <h3>Frond-End Developer</h3>
+        <div className="row">
+          <div className="col-lg-3">
+            <div className="icon-box" data-aos="fade-up">
+              <span>01</span>
+              <h3 className="sub-heading">UI/UX Design</h3>
+              <p>At in proin consequat ut cursus venenatis sapien.</p>
+            </div>
           </div>
-          <div className='button-div'>
-            <Link to='/projects' className='btn' type='button'>Portfolio&nbsp;<i className="fa-solid fa-folder"></i></Link>
-            <a className='cer' href="certificat.pdf" download>Certificat&nbsp;<i className="fa-solid fa-download"></i></a>
+          <div className="col-lg-3">
+            <div className="icon-box" data-aos="fade-up" data-aos-delay="100">
+              <span>02</span>
+              <h3 className="sub-heading">Illustration</h3>
+              <p>At in proin consequat ut cursus venenatis sapien.</p>
+            </div>
+          </div>
+          <div className="col-lg-3">
+            <div className="icon-box" data-aos="fade-up" data-aos-delay="200">
+              <span>03</span>
+              <h3 className="sub-heading">Graphic Design</h3>
+              <p>At in proin consequat ut cursus venenatis sapien.</p>
+            </div>
+          </div>
+          <div className="col-lg-3">
+            <a
+              href="#portfolio"
+              className="btn btn-dark text-uppercase py-4 px-5"
+              data-aos="fade-up"
+              data-aos-delay="300"
+            >
+              View All Work
+            </a>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
